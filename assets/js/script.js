586c9f6deb74b3812701a1806dd9f6cd7db7a4a7
Giarlan Santana <giarlansilva@yahoo.com>
2023-03-04 23:06:56 -0300

    Today it has been created links between pages and access validation by login page using JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
+/*==================== Script of access TRAINING-ROOM page by LOGIN page ====================*/
+
+function sign_in () {
+    let login = document.querySelector ("#user").value;
+    let password = document.querySelector ("#password").value;
+
+    if (login == "gssilva@gmail.com" && password == "123456") {
+
+        location.href = "../html/training-room.html";
+
+    } else {
+        alert ("Verify your User and Password!");
+    }
+
+}
+
+
 
